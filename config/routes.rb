4f79fd6e7d67cba2899e83133b918aa5c69c328d
Philippe Perret <philippe.perret@yahoo.fr>
2018-09-19 20:59:20 +0300

    Petites modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static-pages#help'
+
+  # Concernant les modules d'apprentissage
   get 'abs_modules/index'
   get 'modules/:id' => 'abs_modules#show'
   patch 'modules/:id' => 'abs_modules#update'
   delete 'modules/:id' => 'abs_modules#destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'pages#home'
-
-  get 'home' => 'pages#home'
-
   get   'modules' => 'abs_modules#index'
   # Création d'un module
   post  'modules'  => 'abs_modules#create'
