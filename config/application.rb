593ab91f160e07122ab63b0c96f1a2d49af26416
Philippe Perret <philippe.perret@yahoo.fr>
2018-09-19 14:07:30 +0300

    Ajout de l'accueil

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module IcareRor2019
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Pour définir que le site est en français
+    # Mettre les définitions dans config/locales/fr.yml et autres
+    # Si les locales sont définies dans un autre dossier, utiliser :
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # Pour écrire dans un fichier ERB : <%= t('hello') %>
+    # Ailleurs : i18n.t('hello')
+    config.i18n.default_locale = :fr
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
