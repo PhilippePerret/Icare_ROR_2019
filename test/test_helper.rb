9494044a86aaa7e42831a4ca621facc108adcd74
Philippe Perret <philippe.perret@yahoo.fr>
2018-09-20 09:25:00 +0300

    Ajouts pour testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# Ajouté du tutoriel pour les reporteurs
+require 'minitest/reporters'
+Minitest::Reporters.use!
+# /ajout
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
