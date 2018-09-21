f6235e601c2ebc5a17b08c4432af251794c1d349
Philippe Perret <philippe.perret@yahoo.fr>
2018-09-21 10:44:06 +0300

    Dernier dépôt avant déploiement

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,16 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @page_titre = "Poser sa candidature"
+  end
+
+  test "La page d'inscription présente le bon titre" do
+    get signup_url
+    assert_select 'title', full_title(@page_titre)
+    assert_select 'h2', @page_titre
+  end
+
   test "Un formulaire invalide ne permet pas de s'inscrire" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -21,4 +31,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
   end
   # /test
+
+  test "Un formulaire valide permet une candidature de l'user" do
+    nowi = Time.now.to_i
+    lename = "Le nom à #{nowi}"
+    assert_difference 'User.count', 1 do
+      get signup_url
+      post users_path, params: {
+        user: {
+          name: lename,
+          email: "email#{nowi}@example.com",
+          password: 'mot de passe',
+          password_confirmation: 'mot de passe'
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'static_pages/after_signup'
+    assert_select 'h2', 'Candidature enregistrée'
+    assert_select 'div', /#{lename}, votre candidature/
+  end
+  # test formulaire OK
+
 end
