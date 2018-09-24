a4dab75bdca8151205d70f03cff6069384920d58
Philippe Perret <philippe.perret@yahoo.fr>
2018-09-24 11:02:26 +0300

    Minor changes

--- db/migrate/20180920120042_create_users.rb
+++ db/migrate/20180920120042_create_users.rb
@@ -5,14 +5,14 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string    :name
       t.string    :nom
       t.string    :prenom
-      t.int       :sexe,      limit: 1
-      t.int       :birthyear, limit: 4
+      t.integer   :sexe,      limit: 1
+      t.integer   :birthyear, limit: 4
 
       t.string    :email
       t.string    :password_digest
       t.string    :remember_digest
 
-      t.int       :statut,  default: 0, limit: 2
+      t.integer   :statut,  default: 0, limit: 2
       t.string    :options, default: '00000000'
 
       t.timestamps
